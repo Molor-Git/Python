56a5e7a649508033af6f74544c6b8090983e49c0
Molor <mongolhuu0731@gmail.com>
2021-12-28 17:58:13 -0500

    file.py commented version

---
+++ fundamentals/fundamentals/file.py
@@ -0,0 +1,84 @@
+num1 = 42
+num2 = 2.3
+boolean = True
+string = 'Hello World'
+pizza_toppings = ['Pepperoni', 'Sausage', 'Jalepenos', 'Cheese', 'Olives']
+person = {'name': 'John', 'location': 'Salt Lake', 'age': 37, 'is_balding': False}
+fruit = ('blueberry', 'strawberry', 'banana')
+print(type(fruit))
+print(pizza_toppings[1])
+pizza_toppings.append('Mushrooms')
+print(person['name'])
+person['name'] = 'George'
+person['eye_color'] = 'blue'
+print(fruit[2])
+
+if num1 > 45:
+    print("It's greater")
+else:
+    print("It's lower")
+
+if len(string) < 5:
+    print("It's a short word!")
+elif len(string) > 15:
+    print("It's a long word!")
+else:
+    print("Just right!")
+
+for x in range(5):
+    print(x)
+for x in range(2,5):
+    print(x)
+for x in range(2,10,3):
+    print(x)
+x = 0
+while(x < 5):
+    print(x)
+    x += 1
+
+pizza_toppings.pop()
+pizza_toppings.pop(1)
+
+print(person)
+person.pop('eye_color')
+print(person)
+
+for topping in pizza_toppings:
+    if topping == 'Pepperoni':
+        continue
+    print('After 1st if statement')
+    if topping == 'Olives':
+        break
+
+def print_hello_ten_times():
+    for num in range(10):
+        print('Hello')
+
+print_hello_ten_times()
+
+def print_hello_x_times(x):
+    for num in range(x):
+        print('Hello')
+
+print_hello_x_times(4)
+
+def print_hello_x_or_ten_times(x = 10):
+    for num in range(x):
+        print('Hello')
+
+print_hello_x_or_ten_times()
+print_hello_x_or_ten_times(4)
+
+
+"""
+Bonus section
+"""
+
+print(num3) # NameError: name <variable name> is not defined
+num3 = 72 # num3 has never been called!
+fruit[0] = 'cranberry' # TypeError: 'tuple' object does not support item assignment
+print(person['favorite_team']) # KeyError: 'favorite_team'
+print(pizza_toppings[7]) # IndexError: list index out of range
+    print(boolean) # IndentationError: unexpected indent
+fruit.append('raspberry')# AttributeError: 'tuple' object has no attribute 'append'
+fruit.pop(1) # AttributeError: 'tuple' object has no attribute 'pop'
